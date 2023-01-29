706c6b16494d1812c46367aec2749049c7216003
Ivan Plakhutin <i.plakhutin@gmail.com>
2023-01-29 12:30:40 +0200

    Add Password hasher

--- RSSFeed.Api/Mapper/UserMapperProfile.cs
+++ RSSFeed.Api/Mapper/UserMapperProfile.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
 using RSSFeed.Models.Entities;
 using RSSFeed.Models.Requests;
+using System.Security.Cryptography;
 
 namespace RSSFeed.Api.Mapper
 {
@@ -8,7 +10,14 @@ namespace RSSFeed.Api.Mapper
     {
         public UserMapperProfile()
         {
-            CreateMap<RegisterRequest, User>();
+            CreateMap<RegisterRequest, User>().ForMember(mem => mem.PasswordHash, opt => opt.MapFrom(u => HashPassword(u.Password)));
+        }
+
+        private string HashPassword(string password)
+        {
+            byte[] salt = RandomNumberGenerator.GetBytes(128 / 8);
+            var hash = Convert.ToBase64String(KeyDerivation.Pbkdf2(password, salt, KeyDerivationPrf.HMACSHA256, 100000, 256 / 8));
+            return hash;
         }
     }
 }
